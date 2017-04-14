f3a90f6b050afc804f6cef989353e9cee026eeef
soapej <jonathansoape@gmail.com>
2017-04-14 14:07:04 -0500

    Add files via upload

---
+++ diamond.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+
+  //  PRE HIDDEN ELEMENTS
+
+  // $("header").hide().fadeIn(6000);
+  // $("footer").hide().fadeIn(10000);
+  // $("#lease").css("position", "relative");
+  $("")
+
+
+  // SLIDE IN
+
+  // function slideLeft(){
+  //   $('#lease').css('margin-left', -500px);
+  // }
+
+});
